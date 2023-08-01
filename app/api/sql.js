16829b71fb6e63c4b2ce7911f98e3a7441964522
t.duval <t.duval@ascan.io>
2023-08-01 13:15:26 +0200

    Updating php files path

--- app/api/sql.js
+++ app/api/sql.js
@@ -1,11 +1,11 @@
 import {makeRequest, Methods} from "./MakeRequest";
 
 export const getReviews = (id) => {
-    const url = 'http://localhost:8888/test_technique/get_reviews.php?id=' + id
+    const url = 'https://theophane-duval.fr/php/get_reviews.php?id=' + id
     return makeRequest(url, Methods.GET)
 }
 
 export const getAllArtisans = () => {
-    const url = 'http://localhost:8888/test_technique/get_artisans.php?id=1'
+    const url = 'https://theophane-duval.fr/php/get_artisans.php'
     return makeRequest(url, Methods.GET)
 }
